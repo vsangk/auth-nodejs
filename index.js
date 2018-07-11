dd1637f0e99fabf2ee8717dea4e30784d9ffbc35
Vince Sangkhae <vsangk@gmail.com>
2018-07-10 23:01:18 -0700

    Setup express and routes

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./router');
+
+/** App Setup */
+const app = express();
+// logging middleware
+app.use(morgan('combined'));
+// parses request body to json
+app.use(bodyParser.json({ type: '*/*' }))
+router(app);
+
+/** Server Setup */
+const port = process.env.PORT || 3090;
+// create an http server and forward it to express app
+const server = http.createServer(app)
+server.listen(port);
+console.log('====================================');
+console.log('Server listening on', port);
+console.log('====================================');
