03a4181f999f8838d61a276b95d20f0442c529ff
Vince Sangkhae <vsangk@gmail.com>
2018-07-11 00:42:46 -0700

    Generate JWT on user signup

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,12 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  // sub=subject iat=issued at time
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+}
 
 exports.signup = function(req, res, next) {
   const { email, password } = req.body;
@@ -24,7 +32,7 @@ exports.signup = function(req, res, next) {
     user.save(function(err) {
       if (err) return next(err);
 
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 
