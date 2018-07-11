ee1cd1a8e04cc30332804f212bee68a80ef1e0c4
Vince Sangkhae <vsangk@gmail.com>
2018-07-10 23:39:56 -0700

    Setup mongoose

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./router');
+const mongoose = require('mongoose');
+
+/** DB Setup */
+mongoose.connect('mongodb://localhost/auth');
 
 /** App Setup */
 const app = express();
